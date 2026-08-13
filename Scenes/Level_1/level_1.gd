4acf4c838fcbf539e34ea855c378f399fb913c8a
Vinnie Pauley <vinniepauley@Vinnies-Mac-mini.local>
2026-08-12 23:49:47 -0500

    feat: pellet rendering from tile makers

---
+++ Scenes/Level_1/level_1.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var pellet_scene: PackedScene # < reference pellet.tscn in inspector
+
+@onready var pellet_marker_tiles = %PelletMarkers
+
+func _ready() -> void:
+
+	for cell in pellet_marker_tiles.get_used_cells():
+		var pellet_insert = pellet_scene.instantiate()
+		pellet_insert.position = pellet_marker_tiles.map_to_local(cell)
+		add_child(pellet_insert)
+		print(cell)
+		
+	# no need to show the actual markers
+	pellet_marker_tiles.visible = false
+		
+		
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
