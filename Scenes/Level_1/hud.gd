2eaede6a10e80461c77329e93ff2b750cd764269
Vinnie Pauley <vinniepauley@Vinnies-Mac-mini.local>
2026-08-22 10:44:03 -0500

    feat: countdown frames

--- Scenes/Level_1/hud.gd
+++ Scenes/Level_1/hud.gd
@@ -3,13 +3,36 @@ extends CanvasLayer
 @onready var score_value_indicator = %ScoreValue
 @onready var pause_modal = %PauseModal
 @onready var level_node = get_parent()
+@onready var get_ready_modal = %GetReadyModal
+
+@onready var startup_timer = %StartupTimer
+@onready var countdown_label = %Counter
 
 @onready var resume_button = %ResumeButton
 @onready var quit_button = %QuitButton
 
+var startup_tick_count = 3
+
 func _ready() -> void:
 	resume_button.pressed.connect(_handle_resume_click)
 	quit_button.pressed.connect(_handle_quit_click)
+	countdown_label.text = str(startup_tick_count)
+	startup_timer.wait_time = 1.0
+	startup_timer.one_shot = false
+	startup_timer.timeout.connect(_on_startup_timer_timeout)
+	startup_timer.start()
+
+func _on_startup_timer_timeout() -> void:
+	if startup_tick_count > 1:
+		startup_tick_count -= 1
+		_handle_startup_tick(startup_tick_count)
+		countdown_label.text = str(startup_tick_count)
+	else:
+		startup_timer.stop()
+		get_ready_modal.visible = false
+
+func _handle_startup_tick(tick_number: int) -> void:
+	print("startup tick:", tick_number)
 #
 func _handle_quit_click() -> void:
 	get_tree().paused = false # < un-pause game to allow main menu to work
